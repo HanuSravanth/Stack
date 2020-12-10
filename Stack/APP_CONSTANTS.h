916d8afb1a8712077b16f4fbdac0180e8df57449
Hanu Sravanth T <hanu.sravanth111@gmail.com>
2020-12-10 14:27:00 +0530

    stack functions!

---
+++ Stack/APP_CONSTANTS.h
@@ -0,0 +1,17 @@
+/*
+ * APP_CONSTANTS.h
+ *
+ *  Created on: Dec 10, 2020
+ *      Author: hanus
+ */
+
+#ifndef APP_CONSTANTS_H_
+#define APP_CONSTANTS_H_
+
+#define MAX_DEPTH    32
+#define STACK_OK     1
+#define STACK_EMPTY  2
+#define STACK_FULL   3
+
+
+#endif /* APP_CONSTANTS_H_ */
--- Stack/stack.h
+++ Stack/stack.h
@@ -8,14 +8,31 @@
 #ifndef STACK_H_
 #define STACK_H_
 
+#include "APP_CONSTANTS.h"
 #include <stdint.h>
 
 typedef struct _stack_ Stack;
 struct _stack_
 {
-	int32_t stack[];
-	int32_t top = -1;
+	int32_t data[MAX_DEPTH];
+	int32_t top;
+	uint32_t size;
 };
 
+typedef struct _stack_result_ StackResult;
+struct _stack_result_
+{
+	float data;
+	uint8_t status;
+};
+
+Stack stack_new(uint32_t size);
+uint8_t stack_full(const Stack *stk);
+uint8_t stack_empty(const Stack *stk);
+Stack*  stack_push(Stack *stk,float ele,StackResult *res);
+Stack*  stack_pop(Stack *stk,StackResult *res);
+Stack*  stack_peek(Stack *stk,StackResult *res);
+int8_t  isMatchingPair(char character1, char character2);
+int8_t  areParenthesBalanced(char* exp);
 
 #endif /* STACK_H_ */
